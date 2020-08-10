92bb3e9c9c0bfe39655bf017bbcec9acf4507d4b
咸光金 <yong531315@gmail.com>
2020-08-10 15:20:34 +0800

    1. update 2020年08月10日15:20:29

--- main.c
+++ main.c
@@ -1,10 +1,5 @@
-#include <stdio.h>
-#include <stdint.h>
-#include "cpreprocessor_learn.c"
 
 int main() {
-
-    preprocessorArichmetic();
 
     return 0;
 }
---
+++ type_transformer_learn.c
@@ -0,0 +1,29 @@
+//
+// Created by ysnows on 2020/8/10.
+//
+
+#include <stdio.h>
+
+
+void integerUpdate() {
+    double a = 17;
+    char b = 'c';
+    int sum = a + b;
+
+    printf("sum = %d\n", sum);
+
+}
+
+void example1() {
+    int sum = 100;
+    int count = 9;
+
+    double mean;
+
+    mean = (double) sum / count;
+    printf("mean = %g\n", mean);
+
+
+}
+
+
